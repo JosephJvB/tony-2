5f596d53aad90eb706aeebf5ef2dbfcb8906d3f0
Joseph van Bohemen <joevanbo@protonmail.com>
2023-11-26 17:33:24 +0000

    start getData tests

---
+++ test/unit/getData.test.ts
@@ -0,0 +1,11 @@
+import * as s3 from '../../lambda/s3'
+import * as util from '../../lambda/util'
+import * as youtube from '../../lambda/youtube'
+import * as googleSheets from '../../lambda/googleSheets'
+import * as spotify from '../../lambda/spotify'
+
+describe('getData.ts', () => {
+  describe('#default', () => {
+    it('parsedVideos, youtubeVideos, missingTracks, spotifyPlaylists,', async () => {})
+  })
+})
