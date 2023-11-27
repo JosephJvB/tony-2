beaf5ac0efcebcc7a7bca65092d476083c2c95b4
Joe van Bohemen <joevanbo@protonmail.com>
2023-11-27 17:20:45 +0000

    wip-woops

--- test/unit/spotifyLookups.test.ts
+++ test/unit/spotifyLookups.test.ts
@@ -8,7 +8,7 @@ import {
 } from '../factories'
 
 describe('spotifyLookups.ts', () => {
-  jest.spyOn(console, 'log').mockImplementation(jest.fn())
+  // jest.spyOn(console, 'log').mockImplementation(jest.fn())
   const getByBatchSpy = jest.spyOn(spotifyLookups, 'getByBatch')
   const findTracksSpy = jest.spyOn(spotifyLookups, 'findTracks')
   const getTracksSpy = jest
@@ -18,77 +18,46 @@ describe('spotifyLookups.ts', () => {
     .spyOn(spotify, 'findTrack')
     .mockImplementation(jest.fn())
 
+  // hard 2 mock
   describe('#spotifyLookups.default', () => {
-    it('nextTracks x0, missingTracks x0, byBatch x0, byFind x0', async () => {
-      const numNextTracks = 0
-      const numMissingTracks = 0
-      const numByBatch = 0
-      const numByFind = 0
-
-      const nextTracks = createList(createBestTrack, numNextTracks)
-      const nextMissingTracks = createList(createMissingTrack, numMissingTracks)
-      const foundByBatch = createList(createSpotifyTrack, numByBatch)
-      const foundByFind = createList(createSpotifyTrack, numByFind)
-
-      for (let i = 0; i < numByBatch; i += 50) {
-        getTracksSpy.mockResolvedValueOnce({
-          tracks: foundByBatch.slice(i, i + 50),
-        })
+    it('videoTracks x0, sheetTracks x0, byBatch x0, byFind x0', async () => {
+      const videoTracksConfig = {
+        byBatch: 0,
+        byFind: 0,
+        total: 0,
       }
-      foundByFind.forEach((t) => {
-        findTrackSpy.mockResolvedValueOnce({
-          tracks: {
-            href: '',
-            items: [t],
-          },
-        })
+      const sheetTracksConfig = {
+        byBatch: 0,
+        byFind: 0,
+        total: 0,
+      }
+      const videoTracks = createList(createBestTrack, videoTracksConfig.total)
+      const sheetTracks = createList(
+        createMissingTrack,
+        sheetTracksConfig.total
+      )
+
+      const byBatch: spotify.SpotifyTrack[] = []
+      const byFind: spotify.SpotifyTrack[] = []
+      videoTracks.forEach((t, i) => {
+        if (i < videoTracksConfig.byBatch) {
+          const st = createSpotifyTrack(byBatch.length)
+          t.spotifyId = st.id
+          byBatch.push(st)
+        }
+        if (i < videoTracksConfig.byFind) {
+          byFind.push(createSpotifyTrack(byFind.length))
+        }
+      })
+      sheetTracks.forEach((t, i) => {
+        if (i < sheetTracksConfig.byBatch) {
+          const st = createSpotifyTrack(byBatch.length)
+          t.spotify_ids = st.id
+          byBatch.push(st)
+        }
       })
 
-      const result = await spotifyLookups.default(nextTracks, nextMissingTracks)
-
-      expect(result.spotifyIdMap.size).toBe(numByBatch)
-      expect(result.customIdMap.size).toBe(numByFind)
-
-      expect(getByBatchSpy).toHaveBeenCalledTimes(1)
-      expect(getTracksSpy).toHaveBeenCalledTimes(Math.ceil(numByBatch / 50))
-      expect(findTracksSpy).toHaveBeenCalledTimes(1)
-      expect(findTrackSpy).toHaveBeenCalledTimes(numByFind)
-    })
-
-    it('nextTracks x100, missingTracks x5, byBatch x0, byFind x105', async () => {
-      const numNextTracks = 100
-      const numMissingTracks = 5
-      const numByBatch = 0
-      const numByFind = 105
-
-      const nextTracks = createList(createBestTrack, numNextTracks)
-      const nextMissingTracks = createList(createMissingTrack, numMissingTracks)
-      const foundByBatch = createList(createSpotifyTrack, numByBatch)
-      const foundByFind = createList(createSpotifyTrack, numByFind)
-
-      for (let i = 0; i < numByBatch; i += 50) {
-        getTracksSpy.mockResolvedValueOnce({
-          tracks: foundByBatch.slice(i, i + 50),
-        })
-      }
-      foundByFind.forEach((t) => {
-        findTrackSpy.mockResolvedValueOnce({
-          tracks: {
-            href: '',
-            items: [t],
-          },
-        })
-      })
-
-      const result = await spotifyLookups.default(nextTracks, nextMissingTracks)
-
-      expect(result.spotifyIdMap.size).toBe(numByBatch)
-      expect(result.customIdMap.size).toBe(numByFind)
-
-      expect(getByBatchSpy).toHaveBeenCalledTimes(1)
-      expect(getTracksSpy).toHaveBeenCalledTimes(Math.ceil(numByBatch / 50))
-      expect(findTracksSpy).toHaveBeenCalledTimes(1)
-      expect(findTrackSpy).toHaveBeenCalledTimes(numByFind)
+      // const results = await
     })
   })
 })
