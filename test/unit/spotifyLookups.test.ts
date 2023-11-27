91825b4e040122a27be7ac0de3e596e48d33d222
Joseph van Bohemen <joevanbo@protonmail.com>
2023-11-27 22:16:15 +0000

    no config just vibes

--- test/unit/spotifyLookups.test.ts
+++ test/unit/spotifyLookups.test.ts
@@ -18,46 +18,40 @@ describe('spotifyLookups.ts', () => {
     .spyOn(spotify, 'findTrack')
     .mockImplementation(jest.fn())
 
-  // hard 2 mock
   describe('#spotifyLookups.default', () => {
     it('videoTracks x0, sheetTracks x0, byBatch x0, byFind x0', async () => {
-      const videoTracksConfig = {
-        byBatch: 0,
-        byFind: 0,
-        total: 0,
-      }
-      const sheetTracksConfig = {
-        byBatch: 0,
-        byFind: 0,
-        total: 0,
-      }
-      const videoTracks = createList(createBestTrack, videoTracksConfig.total)
-      const sheetTracks = createList(
-        createMissingTrack,
-        sheetTracksConfig.total
-      )
+      const videoTracks = createList(createBestTrack, 0)
+      const sheetTracks = createList(createMissingTrack, 0)
 
-      const byBatch: spotify.SpotifyTrack[] = []
-      const byFind: spotify.SpotifyTrack[] = []
-      videoTracks.forEach((t, i) => {
-        if (i < videoTracksConfig.byBatch) {
-          const st = createSpotifyTrack(byBatch.length)
-          t.spotifyId = st.id
-          byBatch.push(st)
-        }
-        if (i < videoTracksConfig.byFind) {
-          byFind.push(createSpotifyTrack(byFind.length))
-        }
-      })
-      sheetTracks.forEach((t, i) => {
-        if (i < sheetTracksConfig.byBatch) {
-          const st = createSpotifyTrack(byBatch.length)
-          t.spotify_ids = st.id
-          byBatch.push(st)
-        }
+      const byBatch = createList(createSpotifyTrack, 0)
+      const byFind = createList(createSpotifyTrack, 0)
+
+      for (let i = 0; i < byBatch.length; i += 50) {
+        getTracksSpy.mockResolvedValueOnce({
+          tracks: byBatch.slice(i, i + 50),
+        })
+      }
+      byFind.forEach((t, i) => {
+        findTrackSpy.mockResolvedValueOnce({
+          tracks: {
+            href: `href_${i}`,
+            items: [t],
+          },
+        })
       })
 
-      // const results = await
+      const results = await spotifyLookups.default(videoTracks, sheetTracks)
+
+      expect(results.spotifyIdMap.size).toBe(0)
+      expect(results.customIdMap.size).toBe(0)
+
+      expect(getByBatchSpy).toHaveBeenCalledTimes(1)
+      expect(getByBatchSpy).toHaveBeenCalledWith([])
+      expect(getTracksSpy).toHaveBeenCalledTimes(0)
+
+      expect(findTracksSpy).toHaveBeenCalledTimes(1)
+      expect(findTracksSpy).toHaveBeenCalledWith([])
+      expect(findTrackSpy).toHaveBeenCalledTimes(0)
     })
   })
 })
